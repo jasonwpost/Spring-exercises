878d11eda4330edf75e83b4391e294673e8c12dc
Jason Post <jason.w.post@gmail.com>
2017-03-12 14:32:47 +0100

    constructor di

--- src/com/luv2code/springdemo/MathCoach.java
+++ src/com/luv2code/springdemo/MathCoach.java
@@ -1,10 +1,21 @@
 package com.luv2code.springdemo;
 
 public class MathCoach implements Coach {
+	
+	FortuneService fortuneService;
+	
+	public MathCoach(FortuneService theFortuneService){
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
 		return "Work on two proofs a day";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
 	}
 
 }
