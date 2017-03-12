764d7916512aeac4e8aa3700016478baec7427e7
Jason Post <jason.w.post@gmail.com>
2017-03-12 20:12:13 +0100

    setter and method injection

--- spring-demo-annotations/src/com/post/springdemo/LifeCoach.java
+++ spring-demo-annotations/src/com/post/springdemo/LifeCoach.java
@@ -8,10 +8,28 @@ public class LifeCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
+	// define a default constructor
+	public LifeCoach(){
+		System.out.println(">> inside default constructor");
+	}
+	
+	// define a setter method and use setter injection.
+	// This also works for any method with dependency injection,
+	// which is called 'method injection'. Rather than 'setter 
+	// injection'
+	@Autowired
+	public void setFortuneService(FortuneService theFortuneService){
+		fortuneService = theFortuneService;
+	}
+	
+	
+	/*
+	 * constructor injection example
 	@Autowired
 	public LifeCoach(FortuneService fortuneService){
 		this.fortuneService = fortuneService;
 	}
+	*/
 
 	@Override
 	public String getDailyWorkout() {
