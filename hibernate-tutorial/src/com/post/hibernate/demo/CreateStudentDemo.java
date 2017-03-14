4baa98b20fdd9c2d1ea0850d6f9341edc292f3b0
Jason Post <jason.w.post@gmail.com>
2017-03-14 09:43:42 +0100

    use hibernate to add object to mysql

---
+++ hibernate-tutorial/src/com/post/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,45 @@
+package com.post.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.post.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		
+		// create session factory
+		SessionFactory factory = new Configuration()
+				.configure("hibernate.cfg.xml")
+				.addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			// use session object to save the Java object
+			System.out.println("Creating new Student object");
+			
+			// create a student object
+			Student tempStudent = 
+					new Student("Luke", "Aposilte", "luke@example.com");
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the student object
+			session.save(tempStudent);
+			
+			// commit the transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		} finally {
+			factory.close();
+		}
+	}
+
+}
